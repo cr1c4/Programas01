7559db68f0e1a68c15b3b1efbd175fd0fb6cd623
Crica <chernandez3.14@outlook.com>
2021-06-06 11:42:10 -0600

    Ejercio de Operaradores logicos

---
+++ operadoresLogicos.py
@@ -0,0 +1,10 @@
+print ("OPERADORES LOGICOS\n\n")
+num_uno = int(input("Digitar un numero: "))
+if num_uno == 5 and num_uno >= 5:
+    print ("Se cumplieron las dos condiciones")
+if num_uno == 5 or num_uno >= 5:
+    print ("Se cumplio al menos una condicion")
+else:
+    print ("El valor digitado no es correcto")
+
+print ("Fin.")
