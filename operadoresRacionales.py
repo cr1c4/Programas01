1a7243d3fdd6b40c17b29f952ffd1c38c7d4a4be
Crica <chernandez3.14@outlook.com>
2021-06-06 02:05:59 -0600

    nuevo programa operadores racionales

---
+++ operadoresRacionales.py
@@ -0,0 +1,18 @@
+print ("Introduce dos numeros a comparar: \n")
+num_uno = int(input("Introduce el primer numero: "))
+num_dos = int(input("Introduce el segundo numero: "))
+
+print ("\nLos numeros a comparar son: ",num_uno," y ",num_dos,"\n")
+
+if num_uno == num_dos:
+    print ("Es igual...")
+if num_uno != num_dos:
+    print ("Es diferente..")
+if num_uno < num_dos:
+    print ("Es menor..")
+if num_uno > num_dos:
+    print ("Es mayor..")
+if num_uno <= num_dos:
+    print ("Es menor o igual..")
+if num_uno >= num_dos:
+    print ("Es mayor o igual..")
