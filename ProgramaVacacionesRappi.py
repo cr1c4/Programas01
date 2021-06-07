82e07daa02b1aecd8b7b2bbacab8c3f93674d5be
Crica <chernandez3.14@outlook.com>
2021-06-07 01:15:58 -0600

    primera prueba

--- ProgramaVacacionesRappi.py
+++ ProgramaVacacionesRappi.py
@@ -1,47 +1,56 @@
-#Este en un programa de control de Vacaciones.
+#Este en un programa de control.
 print ("""\n\n*|=================================|*
  |---PROGRAMA CONTROL VACACIONES---|
 *|=================================|*\n\n"""
 )
 
 #Solicitar la clave del departamento
-nombre = input("Por favor digite su nombre: ")
 clave = int(input("""\n\n1: Clave1 <Departamento:Servicio al cliente>
 2: Clave2 <Departamento:Logistica>
 3: Clave1 <Departamento:Gerencia>\n
 Por favor digite la Clave del departamento: """
 ))
 
-#Solicitar los años
+
+
+#Solicitar datos
+nombre = input("\nPor favor digite su nombre: ")
 tiempo = int(input("\nPor digite la cantidad de años laborales: "))
 
 #verificar si es clave 1
 if clave == 1:
     if tiempo == 1:
-        print ("\nMensaje: 6 dias de vacaciones")
+        DatoVacaciones = "\n6 dias"
     elif tiempo >=2 and tiempo <=6:
-        print ("\nMensaje: 14 dias de vacaciones")
+        DatoVacaciones = "\n14 dias"
     elif tiempo >= 7:
-        print ("\nMensaje: 20 dias de vacaciones")
+        DatoVacaciones = "\n20 dias"
     else:
-        print ("\nSin derecho a Vacaciones")  
+        DatoVacaciones = "\nSin derecho a vacaciones"  
 elif clave == 2:
     if tiempo == 1:
-        print ("\nMensaje: 7 dias de vacaciones")
+        DatoVacaciones = "7 dias"
     elif tiempo >=2 and tiempo <=6:
-        print ("\nMensaje: 15 dias de vacaciones")
+        DatoVacaciones = "15 dias"
     elif tiempo >= 7:
-        print ("\nMensaje: 22 dias de vacaciones")
+        DatoVacaciones = "22 dias"
     else:
-        print ("\nSin derecho a Vacaciones")  
+        DatoVacaciones = "Sin derecho a vacaciones"  
 elif clave == 3:
     if tiempo == 1:
-        print ("\nMensaje: 10 dias de vacaciones")
+        DatoVacaciones = "10 dias"
     elif tiempo >=2 and tiempo <=6:
-        print ("\nMensaje: 20 dias de vacaciones")
+        DatoVacaciones = "20 dias"
     elif tiempo >= 7:
-        print ("\nMensaje: 30 dias de vacaciones")
+        DatoVacaciones = "30 dias"
     else:
-        print ("\nSin derecho a Vacaciones")  
+        DatoVacaciones = "Sin derecho a vacaciones"
 else:
-    print ("El dato no es correcto")
+    print ("\nEl valor de departamento no es correcto")
+
+if clave == 1 or clave == 3 or clave == 3:
+    print ("\n\n============Resultados============\nEmpleado: ",nombre)
+    print ("Antiguedad: ",tiempo," años")
+    print ("Vacaciones disponibles por: ",DatoVacaciones,"\n\n")
+
+   
