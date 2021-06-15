a0d625641cb665a3a38a0c2dc8a8d3db2a3e4064
Crica <chernandez3.14@outlook.com>
2021-06-14 23:09:33 -0600

    test

--- ProgramaBucle.py
+++ ProgramaBucle.py
@@ -1,5 +1,17 @@
-x=1
-while x < 5:
-    print(x)
-    x+=1
-print("Fin")
+x,y=0,1
+while x <= 1597:
+    print (x,y,end=" ")
+    x=x+y
+    y=x+y
+print ("Fin")
+
+import os
+
+#Programa para calcular el valor de PI
+k,r=0,0
+while True:
+    r += 4 * pow(-1, k) / (2 * k + 1)
+    k += 1
+    os.system("Clear")
+    print (k)
+    print (r)
