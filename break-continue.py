f1f27855aea7a79ad1fe6a80fce0cf6bb1997c87
Crica <chernandez3.14@outlook.com>
2021-06-16 22:55:45 -0600

    programa break

---
+++ break-continue.py
@@ -0,0 +1,11 @@
+#ejemplo para break
+print ("while con la sentecia breack \n")
+contador = 0
+while contador < 10:
+    contador += 1
+
+    if contador == 5:
+        break
+    print ("Valor actual de la variable: ", contador)
+    
+print ("Fin del programa, la sentencia breakse ha ejecutado")
